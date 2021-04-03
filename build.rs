2a570553cecb86f7061084816de0368391971f5a
Magicloud <1886157+Magicloud@users.noreply.github.com>
2021-04-03 13:24:46 +0800

    Fancy not working way

--- build.rs
+++ build.rs
@@ -1,16 +1,7 @@
 fn main() {
     windows::build!(
-        Windows::Win32::WindowsAndMessaging::{
-            SYSTEM_PARAMETERS_INFO_ACTION,
-            SystemParametersInfo_fWinIni,
-            SystemParametersInfoW
-        },
-        Windows::Foundation::{
-            IAsyncAction,
-            IAsyncOperation,
-            AsyncActionCompletedHandler
-        },
+        Windows::Foundation::IAsyncOperation,
         Windows::Storage::StorageFile,
-        Windows::System::UserProfile::LockScreen
+        Windows::System::UserProfile::UserProfilePersonalizationSettings
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,8 @@ mod bindings {
 }
 
 use bindings::{
-    Windows::Win32::WindowsAndMessaging::{
-        SYSTEM_PARAMETERS_INFO_ACTION,
-        SystemParametersInfo_fWinIni,
-        SystemParametersInfoW
-    },
     Windows::Storage::StorageFile,
-    Windows::System::UserProfile::LockScreen
+    Windows::System::UserProfile::UserProfilePersonalizationSettings
 };
 
 use reqwest::blocking::get;
@@ -18,41 +13,33 @@ use sxd_xpath::evaluate_xpath;
 use std::fs::File;
 use std::io::copy;
 use std::env::var;
-use std::ffi::OsString;
-use std::iter::once;
-use std::os::windows::ffi::OsStrExt;
 
-fn update_wallpaper(pic_filename : &String) -> Result<(), Box<dyn std::error::Error>> {
-    let pic = OsString::from(pic_filename).as_os_str().encode_wide().chain(once(0)).collect::<Vec<u16>>();
-    unsafe {
-        SystemParametersInfoW(SYSTEM_PARAMETERS_INFO_ACTION::SPI_SETDESKWALLPAPER,
-            0, pic.as_ptr() as *mut _,
-            SystemParametersInfo_fWinIni::SPIF_SENDWININICHANGE)
-    };
-    Ok(())
-}
-
-async fn update_lockscreen_image(pic_filename : String) -> Result<(), Box<dyn std::error::Error>> {
+async fn set_all(pic_filename : String) -> Result<(), Box<dyn std::error::Error>> {
     let file = StorageFile::GetFileFromPathAsync(pic_filename)?.await?;
-    LockScreen::SetImageFileAsync(file)?.await?;
+    let upps = UserProfilePersonalizationSettings::Current()?;
+    upps.TrySetWallpaperImageAsync(&file)?.await?;
+    upps.TrySetLockScreenImageAsync(&file)?.await?;
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // idx is from today back.
-    // mkt : en-US,zh-CN,ja-JP,en-AU,en-UK,de-DE,en-NZ,en-CA
-    let page_url = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=zh-CN";
-    let pic_file_path = format!("{}\\wallpaper.jpg", var("TEMP")?);
-    let body = get(page_url)?.text()?;
-    let pkg = parse(&body)?;
-    let doc = pkg.as_document();
-    let value = evaluate_xpath(&doc, "//urlBase/text()")?;
-    let pic_url = format!("http://www.bing.com{}_1920x1080.jpg", value.string());
-    let img_bytes = get(pic_url)?.bytes()?;
-    let mut data = img_bytes.as_ref();
-    let mut f = File::create(&pic_file_path)?;
-    copy(&mut data, &mut f)?;
-    futures::executor::block_on(update_lockscreen_image(pic_file_path.clone()))?;
-    update_wallpaper(&pic_file_path)?;
-    Ok(())
+    if UserProfilePersonalizationSettings::IsSupported()? {
+        // idx is from today back.
+        // mkt : en-US,zh-CN,ja-JP,en-AU,en-UK,de-DE,en-NZ,en-CA
+        let page_url = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=zh-CN";
+        let pic_file_path = format!("{}\\wallpaper.jpg", var("TEMP")?);
+        let body = get(page_url)?.text()?;
+        let pkg = parse(&body)?;
+        let doc = pkg.as_document();
+        let value = evaluate_xpath(&doc, "//urlBase/text()")?;
+        let pic_url = format!("http://www.bing.com{}_1920x1080.jpg", value.string());
+        let img_bytes = get(pic_url)?.bytes()?;
+        let mut data = img_bytes.as_ref();
+        let mut f = File::create(&pic_file_path)?;
+        copy(&mut data, &mut f)?;
+        futures::executor::block_on(set_all(pic_file_path))?;
+        Ok(())
+    } else {
+        Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "not supported")))
+    }
 }
